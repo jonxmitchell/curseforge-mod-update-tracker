ef72395d9c4c2fcc7884cd454864a68e2476c586
Jon Mitchell <jonxmitchell@outlook.com>
2024-07-07 13:08:55 +0100

    removed font awesome and using flowbite for icons

--- src/renderer/components/ModList.js
+++ src/renderer/components/ModList.js
@@ -23,12 +23,16 @@ function renderModList(mods) {
                     <a href="${
 											mod.website_url
 										}" target="_blank" class="icon-button">
-                        <i class="fas fa-external-link-alt"></i>
+                        <svg class="w-4 h-4" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 18 18">
+                            <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M15 11v4.833A1.166 1.166 0 0 1 13.833 17H2.167A1.167 1.167 0 0 1 1 15.833V4.167A1.166 1.166 0 0 1 2.167 3h4.618m4.447-2H17v5.768M9.111 8.889l7.778-7.778"/>
+                        </svg>
                     </a>
                     <button class="icon-button delete-mod" data-mod-id="${
 											mod.mod_id
 										}">
-                        <i class="fas fa-trash"></i>
+                        <svg class="w-4 h-4" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 18 20">
+                            <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M1 5h16M7 8v8m4-8v8M7 1h4a1 1 0 0 1 1 1v3H6V2a1 1 0 0 1 1-1ZM3 5h12v13a1 1 0 0 1-1 1H4a1 1 0 0 1-1-1V5Z"/>
+                        </svg>
                     </button>
                 </div>
             </div>
--- src/renderer/components/WebhookList.js
+++ src/renderer/components/WebhookList.js
@@ -20,13 +20,19 @@ function renderWebhookList(webhooks) {
                 </div>
                 <div class="flex space-x-2">
                     <button class="icon-button show-full-webhook">
-                        <i class="fas fa-expand-alt"></i>
+                        <svg class="w-4 h-4" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 20 20">
+                            <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M15 13.407V6.593a.5.5 0 0 0-.854-.353L9.6 10l4.546 3.76A.5.5 0 0 0 15 13.407Zm-5 0V6.593a.5.5 0 0 0-.854-.353L4.6 10l4.546 3.76A.5.5 0 0 0 10 13.407Z"/>
+                        </svg>
                     </button>
                     <button class="icon-button test-webhook">
-                        <i class="fas fa-vial"></i>
+                        <svg class="w-4 h-4" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 20 20">
+                            <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M5 4a4 4 0 0 1 4 4v6M5 4a4 4 0 0 0-4 4v6h8M5 4h9M9 14h10V8a3.999 3.999 0 0 0-4-4h-2m4 0v3m3 0v3m-6 4v3"/>
+                        </svg>
                     </button>
                     <button class="icon-button delete-webhook" data-webhook-id="${webhook.id}">
-                        <i class="fas fa-trash"></i>
+                        <svg class="w-4 h-4" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 18 20">
+                            <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M1 5h16M7 8v8m4-8v8M7 1h4a1 1 0 0 1 1 1v3H6V2a1 1 0 0 1 1-1ZM3 5h12v13a1 1 0 0 1-1 1H4a1 1 0 0 1-1-1V5Z"/>
+                        </svg>
                     </button>
                 </div>
             </div>
@@ -39,10 +45,18 @@ function renderWebhookList(webhooks) {
 			const webhookUrlElement = webhookElement.querySelector(".webhook-url");
 			if (webhookUrlElement.textContent === truncatedUrl) {
 				webhookUrlElement.textContent = webhook.url;
-				showFullWebhookButton.innerHTML = '<i class="fas fa-compress-alt"></i>';
+				showFullWebhookButton.innerHTML = `
+                    <svg class="w-4 h-4" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 20 20">
+                        <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M5 13.407V6.593a.5.5 0 0 1 .854-.353L10.4 10l-4.546 3.76A.5.5 0 0 1 5 13.407Zm5 0V6.593a.5.5 0 0 1 .854-.353L15.4 10l-4.546 3.76A.5.5 0 0 1 10 13.407Z"/>
+                    </svg>
+                `;
 			} else {
 				webhookUrlElement.textContent = truncatedUrl;
-				showFullWebhookButton.innerHTML = '<i class="fas fa-expand-alt"></i>';
+				showFullWebhookButton.innerHTML = `
+                    <svg class="w-4 h-4" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 20 20">
+                        <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M15 13.407V6.593a.5.5 0 0 0-.854-.353L9.6 10l4.546 3.76A.5.5 0 0 0 15 13.407Zm-5 0V6.593a.5.5 0 0 0-.854-.353L4.6 10l4.546 3.76A.5.5 0 0 0 10 13.407Z"/>
+                    </svg>
+                `;
 			}
 		});
 
