966e00a28c4b1e7b64ab1382d9670a6cb6dcc441
Jon Mitchell <jonxmitchell@outlook.com>
2024-07-10 12:17:37 +0100

    filed mod tracker tab hover effect displaying correctly

--- src/renderer/templates/tabNavigationHTML.js
+++ src/renderer/templates/tabNavigationHTML.js
@@ -3,7 +3,7 @@ module.exports = function tabNavigation() {
     <div class="mb-4 border-b border-gray-200 dark:border-gray-700">
       <ul class="flex flex-wrap -mb-px text-sm font-medium text-center" id="myTab" role="tablist">
         <li class="mr-2" role="presentation">
-          <button class="inline-flex items-center justify-center p-4 border-b-2 rounded-t-lg" id="mods-tab" data-tabs-target="#mods" type="button" role="tab" aria-controls="mods" aria-selected="false">
+          <button class="inline-flex items-center justify-center p-4 border-b-2 border-transparent rounded-t-lg hover:text-gray-600 hover:border-gray-300 dark:hover:text-gray-300" id="mods-tab" data-tabs-target="#mods" type="button" role="tab" aria-controls="mods" aria-selected="false">
             <svg class="w-4 h-4 mr-2" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 20 20">
               <path d="M10 0a10 10 0 1 0 10 10A10.011 10.011 0 0 0 10 0Zm0 5a3 3 0 1 1 0 6 3 3 0 0 1 0-6Zm0 13a8.949 8.949 0 0 1-4.951-1.488A3.987 3.987 0 0 1 9 13h2a3.987 3.987 0 0 1 3.951 3.512A8.949 8.949 0 0 1 10 18Z" />
             </svg>
