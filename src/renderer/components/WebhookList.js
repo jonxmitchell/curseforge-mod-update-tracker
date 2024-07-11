30cbf52f30afee71466194cfce5186adde713b41
Jon Mitchell <jonxmitchell@outlook.com>
2024-07-11 22:38:59 +0100

    changed compress icon

--- src/renderer/components/WebhookList.js
+++ src/renderer/components/WebhookList.js
@@ -56,7 +56,7 @@ function renderWebhookList(webhooks) {
 				webhookUrlElement.textContent = webhook.url;
 				showFullWebhookButton.innerHTML = `
                     <svg class="w-5 h-5" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24">
-                        <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M4 8h4V4m12 4h-4V4M4 16h4v4m12-4h-4v4"/>
+                        <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M5 15h4m0 0v4m0-4-5 5m15-5h-4m0 0v4m0-4 5 5M5 9h4m0 0V5m0 4L4 4m15 5h-4m0 0V5m0 4 5-5"/>
                     </svg>
                 `;
 			} else {
