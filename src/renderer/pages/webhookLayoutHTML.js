35f6c7239d8b4c6103c3f7d1dd61229202b606b2
Jon Mitchell <jonxmitchell@outlook.com>
2024-07-11 16:57:56 +0100

    fixed an tailwind styling issue format

--- src/renderer/pages/webhookLayoutHTML.js
+++ src/renderer/pages/webhookLayoutHTML.js
@@ -114,7 +114,7 @@ module.exports = function webhookLayout() {
                     <span class="sr-only">Add list</span>
                   </button>
                   <button type="button" class="p-2 text-gray-500 rounded cursor-pointer hover:text-gray-900 hover:bg-gray-100 dark:text-gray-400 dark:hover:text-white dark:hover:bg-gray-600" data-format="code">
-                    <svg class="w-4 h-4" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 024 24">
+                    <svg class="w-4 h-4" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24">
                       <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="m8 8-4 4 4 4m8 0 4-4-4-4m-2-3-4 14"/>
                     </svg>
                     <span class="sr-only">Code block</span>
