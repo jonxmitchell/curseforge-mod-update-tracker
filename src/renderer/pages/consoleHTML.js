54cfd2dea2e81f18f3ba10e0a8ad51b7d78c4676
Jon Mitchell <jonxmitchell@outlook.com>
2024-07-12 16:02:35 +0100

    fixed the console appearing under other tabs

--- src/renderer/pages/consoleHTML.js
+++ src/renderer/pages/consoleHTML.js
@@ -2,8 +2,10 @@ module.exports = function console() {
 	return `
     <div class="hidden p-4 rounded-lg" id="console" role="tabpanel" aria-labelledby="console-tab">
       <h3 class="text-xl font-bold mb-4">Console</h3>
-      <div id="consoleOutput" class="bg-darker-surface text-gray-300 p-4 rounded overflow-y-auto font-mono text-sm mb-4 border border-gray-700" style="height: 400px;"></div>
-      <button id="clearConsoleButton" class="custom-button">Clear Console</button>
+      <div id="consoleContainer" class="console-container">
+        <div id="consoleOutput" class="bg-darker-surface text-gray-300 p-4 rounded overflow-y-auto font-mono text-sm mb-4 border border-gray-700"></div>
+        <button id="clearConsoleButton" class="custom-button">Clear Console</button>
+      </div>
     </div>
   `;
 };
