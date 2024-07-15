f8e8f398c40306865e9ab7ba453b4ecf529f1364
Jon Mitchell <jonxmitchell@outlook.com>
2024-07-15 20:05:42 +0100

    added open logs button

--- src/renderer/pages/consoleHTML.js
+++ src/renderer/pages/consoleHTML.js
@@ -4,7 +4,10 @@ module.exports = function console() {
       <h3 class="text-xl font-bold mb-4">Console</h3>
       <div id="consoleContainer" class="console-container">
         <div id="consoleOutput" class="bg-darker-surface text-gray-300 p-4 rounded overflow-y-auto font-mono text-sm mb-4 border border-gray-700"></div>
-        <button id="clearConsoleButton" class="custom-button">Clear Console</button>
+        <div class="flex justify-between">
+          <button id="clearConsoleButton" class="custom-button">Clear Console</button>
+          <button id="openLogsFolder" class="custom-button">Open Logs Folder</button>
+        </div>
       </div>
     </div>
   `;
